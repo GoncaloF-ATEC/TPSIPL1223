fb5ddbdd4f5737d427212e7e48f4d38c91ec46fb
GoncaloF <Goncalo.Feliciano.0001150@edu.atec.pt>
2023-12-12 23:05:38 +0000

    ex respostas
    Faça um Programa que mostre a mensagem "Alo mundo" na tela.
    Faça um Programa que com um número e então mostre a mensagem
        O número é [número].
    Faça um Programa com dois números e imprima a soma.
    Faça um Programa com 4 notas bimestrais e mostre a média.

---
+++ 0789 - Fundamentos de linguagem JAVA/Semana1/demo1/ex1.py
@@ -0,0 +1,39 @@
+#Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+print("Ola Mundo")
+
+
+#Faça um Programa que com um número e então mostre a mensagem "O número é [número]".
+num = 10
+print("O número é", num)
+print(f"O número é {num}")
+
+
+#Faça um Programa com dois números e imprima a soma.
+num1 = 10
+num2 = 14
+
+soma = num1 + num2
+"""
+soma = num1 + 14
+soma = 10 + 14
+soma = 24
+"""
+
+myStr = ""  # str norma, aprans txt
+myFStr = f"" # str formatada, str com capacidade de ter var no meio
+
+print(f"A soma de {num1} com {num2} é {soma}")
+
+
+
+#Faça um Programa com 4 notas bimestrais e mostre a média.
+
+val1 = 10
+val2 = 14
+var3 = 19
+val4 = 5
+
+soma = val1 + val2 + var3 + val4
+
+media = soma / 4
+print(f"a meida é {media}")
