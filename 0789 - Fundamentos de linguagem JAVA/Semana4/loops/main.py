8883c989f2c40508c54d75d125ab4652fdc12036
GoncaloF <Goncalo.Feliciano.0001150@edu.atec.pt>
2024-01-09 17:22:43 +0000

    funções

--- 0789 - Fundamentos de linguagem JAVA/Semana4/loops/main.py
+++ 0789 - Fundamentos de linguagem JAVA/Semana4/loops/main.py
@@ -9,3 +9,8 @@ utl.ola_mundo3("Gonçalo")
 utl.ola_mundo4("Gonçalo")
 
 
+txt = utl.ola_mundo5("Gonçalo", 2023)
+
+print("a meg é:")
+print(txt)
+print("fim da msg ")
--- 0789 - Fundamentos de linguagem JAVA/Semana4/loops/utils.py
+++ 0789 - Fundamentos de linguagem JAVA/Semana4/loops/utils.py
@@ -6,9 +6,14 @@ def ola_mundo2():
     print("Ola Mundo2")
 
 
-
 def ola_mundo3(nome):
     print(f"Ola Mundo3 {nome}")
 
+
 def ola_mundo4(nome: str): # nome: type hint
     print(f"Ola Mundo3 {nome}")
+
+
+def ola_mundo5(nome: str, ano: int):
+    msg = f"Ola Mundo3 {nome}, ano: {ano}"
+    return msg
