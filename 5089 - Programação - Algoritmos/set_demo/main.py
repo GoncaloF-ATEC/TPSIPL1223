52f4ec47b31273bac2a847fccebfc3a94af1d976
Goncalo Feliciano <goncaloaaf@gmail.com>
2024-06-20 18:14:30 +0100

    ex

--- 5089 - Programação - Algoritmos/set_demo/main.py
+++ 5089 - Programação - Algoritmos/set_demo/main.py
@@ -1,11 +1,47 @@
 
 """
 
-ver se uma lista de num contem valores duplicados
-
+ver se uma lista num contem valores duplicados
 
 """
 
 
-def ex1(lista:list):
+def ex1(lista: list):
+    aux = set()
+
+    for i in lista:
+        if aux.__contains__(i):
+            return True
+        aux.add(i)
+
+    return False
+
+
+
+"""
+
+tendo uma lista ordenada  encontre os dois valores da lista q a sua soma seja t_sum,
+ pode assumir que:
+    todos os valores da lista são inteiros e a lista nunca esta vazia 
+    eixtem dois valores que somados sao t_sum
+    apenas eixtem dois valores que somados sao t_sum
+    
+"""
+
+def ex2(lista: list, t_sum: int):
+    pass
+
+
+"""
+
+tendo uma lista não ordenada  encontre os dois valores da lista q a sua soma seja t_sum,
+ pode assumir que:
+    todos os valores da lista são inteiros e a lista nunca esta vazia 
+    eixtem dois valores que somados sao t_sum
+    apenas eixtem dois valores que somados sao t_sum
+
+
+"""
+
+def ex3(lista: list, t_sum: int):
     pass
--- 5089 - Programação - Algoritmos/set_demo/teste.py
+++ 5089 - Programação - Algoritmos/set_demo/teste.py
@@ -1,10 +1,18 @@
 import unittest
 import main as m
+import time as t
 
 
 class MyTestCase(unittest.TestCase):
     lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     lista2 = [1, 2, 3, 4, 5, 6, 7, 8, 1, 9, 10]
+
+    def test_isEmpty_noDups(self):
+        self.assertGreater(len(self.lista), 0)
+
+    def test_isEmpty_test_withDups(self):
+        self.assertGreater(len(self.lista2), 0)
+
 
     def test_noDups(self):
         self.assertEqual(m.ex1(self.lista), False)
